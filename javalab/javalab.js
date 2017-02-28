f11797f7546a51730340934372760bfa40904058
dnonnweiler <dnonnweiler@students.mchenry.edu>
2017-02-27 20:49:33 -0600

    Add files via upload

---
+++ javalab/javalab.js
@@ -0,0 +1,24 @@
+function change01() {
+document.getElementById("full").src ="images/deflarge.jpg";
+}
+
+function change02() {
+document.getElementById("full").src ="images/eruditelarge.jpg";
+}
+
+function change03() {
+document.getElementById("full").src ="images/gnomelarge.jpg";
+}
+
+function change04() {
+document.getElementById("full").src ="images/iksarlarge.jpg";
+}
+
+function change05() {
+document.getElementById("full").src ="images/ogrelarge.jpg";
+}
+
+function change06() {
+document.getElementById("full").src ="images/trolllarge.jpg";
+}
+
