e5ea9f32280336d1c265222bf3e66bd1e69303b7
dnonnweiler <dnonnweiler@students.mchenry.edu>
2017-03-01 21:46:22 -0600

    Add files via upload

---
+++ phplab/precious.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <link rel="stylesheet" href="flexphp.css" type="text/css">
+    <meta charset="UTF-8">
+    <title>PHP lab</title>
+</head>
+<body>
+  <div id="wrapper">
+   <header>
+      
+       <?php require ("header.php"); ?>
+      
+   </header>
+   
+    <main>
+       <div class="content">
+        <h1>Precious Metals</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perspiciatis molestiae obcaecati ipsa sunt fugiat voluptate voluptatibus, quaerat modi quos saepe illum perferendis doloremque aliquam explicabo, distinctio, alias pariatur animi quia est. Nobis illo eos voluptate soluta, libero obcaecati consectetur alias quia veritatis, numquam quos aut sequi doloremque laudantium rerum dolor! Rem dignissimos voluptates, voluptatum nulla, blanditiis, assumenda, perspiciatis ducimus distinctio eos omnis fuga? Nesciunt animi, natus, amet eius porro vel optio culpa ullam rem dignissimos possimus sit! Perspiciatis rerum voluptatibus ducimus quisquam unde iusto necessitatibus soluta illo minima at eius quidem optio molestiae voluptates provident excepturi repellat impedit eveniet, est magnam, quibusdam repellendus eum minus maiores. Aut omnis quia vero quasi doloremque sapiente voluptates repellat repudiandae nesciunt cupiditate! Nemo ipsa assumenda, esse cumque voluptatem velit?</p>
+        </div>
+       
+    </main>
+    
+    
+    <footer>
+    <div class="footer">
+       
+    <?php
+        include "footer.php"; 
+    ?>
+    
+    </div>
+     </footer>
+     
+    </div>
+</body>
+</html>
